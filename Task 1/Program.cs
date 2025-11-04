d9459a5f616340f5e7e967e2d0e4e4e462eb29dd
Prince <princeshrestha2111018@gmail.com>
2025-11-04 15:21:08 +0545

    Add project files.

---
+++ Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Declare a variable named userName to store your name and initialize it.
+string userName = "Prince Shrestha";
+
+//Declare a variable named luckyNumber to store an integer and initialize it to your favorite single-digit number.
+int luckyNumber = 3;
+
+//Use string interpolation to print a single line of output to the console that says: "Hello, [userName]! Your lucky number is [luckyNumber] ."
+Console.WriteLine($"Hello, {userName}!, Your lucky number is {luckyNumber}.");
---
+++ Task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Create a class named Circle.
+class Circle
+
+{
+    //Inside the class, declare a constant variable named PI and initialize it with the value 3.14
+    public const double PI = 3.14;
+
+    //Try to modify the value of PI of the Circle class and observe the compilation error and explain the error. 
+    public void ModifyPI()
+    {
+        PI = 3.9999;
+    }
+}
